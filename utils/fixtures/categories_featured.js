e47d0b607633bed56436046271785cd6a0554fd1
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-01-15 16:36:00 +0800

    Added fixtures for categories

---
+++ utils/fixtures/categories_featured.js
@@ -0,0 +1,22 @@
+
+
+module.exports = [{
+  name: "School Supplies",
+  description: "School Supplies",
+  categoryCode: "01",
+  isFeatured: true,
+  images: [{
+    imageUrl: "http://imagineleeland.org/wp-content/uploads/2017/08/school-supplies.jpg",
+    imageType: "BACKGROUND"
+  }]
+}, {
+  name: "Electronics",
+  description: "Electronics",
+  categoryCode: "02",
+  isFeatured: true,
+  images: [{
+    imageUrl: "http://imagineleeland.org/wp-content/uploads/2017/08/school-supplies.jpg",
+    imageType: "BACKGROUND"
+  }]
+}
+]
