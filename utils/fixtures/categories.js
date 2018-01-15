bd9ad11733bc8acd297ee75d4cb0cd51695c6d97
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2018-01-15 16:43:18 +0800

    Added categories fixture

--- utils/fixtures/categories.js
+++ utils/fixtures/categories.js
@@ -1,117 +1,8 @@
 module.exports = [
   {
-    "id": 1,
-    "name": "School Supplies",
-    "description": "School Supplies",
-    "categoryCode": "01",
-    "dateCreated": "2016-05-17 18:13:37",
-    "lastUpdated": "2017-10-25 10:44:05",
-    "createdBy": "711admin",
-    "updatedBy": "admin",
-    "isFeatured": true,
-    "images": [
-        {
-            "imageUrl": "http://imagineleeland.org/wp-content/uploads/2017/08/school-supplies.jpg",
-            "imageType": "BACKGROUND"
-        }
-    ]
-  },
-  {
-    "id": 3,
-    "name": "Electronics",
-    "description": "Electronics",
-    "categoryCode": "03",
-    "dateCreated": "2016-12-01 09:38:47",
-    "lastUpdated": "2017-10-17 15:00:21",
-    "createdBy": "711admin",
-    "updatedBy": "admin",
-    "isFeatured": true,
-    "images": [
-        {
-            "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Circuit_elements.svg/400px-Circuit_elements.svg.png",
-            "imageType": "BACKGROUND"
-        }
-    ]
-  },
-  {
-    "id": 5,
-    "name": "Accessories",
-    "description": "Accessories",
-    "categoryCode": "05",
-    "dateCreated": "2017-06-13 09:48:51",
-    "lastUpdated": "2017-10-17 16:08:21",
-    "createdBy": "711admin",
-    "updatedBy": "admin",
-    "isFeatured": true,
-    "images": [
-        {
-            "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/accessories-bg.jpg",
-            "imageType": "BACKGROUND"
-        },
-        {
-            "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/accessories-main.svg",
-            "imageType": "MAIN"
-        },
-        {
-            "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/accessories-nav.svg",
-            "imageType": "NAV_ICON"
-        }
-    ]
-  },
-  {
-    "id": 11,
-    "name": "On-the-go",
-    "description": "On-the-go",
-    "categoryCode": "11",
-    "dateCreated": "2017-06-14 16:58:36",
-    "lastUpdated": "2017-10-17 14:58:43",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
-    "isFeatured": true,
-    "images": [
-        {
-            "imageUrl": "https://www.emotivebrand.com/wp-content/uploads/2013/12/publicinformationsymbol_emergencyexit.jpg",
-            "imageType": "BACKGROUND"
-        }
-    ]
-  },
-  {
-    "id": 20,
-    "name": "Miscellaneous",
-    "description": "Miscellaneous",
-    "categoryCode": "20",
-    "dateCreated": "2017-10-17 15:23:14",
-    "lastUpdated": "2017-11-20 18:13:26",
-    "createdBy": "admin",
-    "updatedBy": "admin",
-    "isFeatured": true,
-    "images": [
-        {
-            "imageUrl": "https://cliqq.imgix.net/00001.png",
-            "imageType": "MAIN"
-        }
-    ]
-  },
-  {
-    "id": 2,
-    "name": "Non-food",
-    "description": "Non-food",
-    "categoryCode": "02",
-    "dateCreated": "2016-07-27 10:27:52",
-    "lastUpdated": "2016-07-27 10:27:52",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
-    "isFeatured": false
-  },
-  {
-    "id": 4,
     "name": "Apparel",
     "description": "Apparel",
     "categoryCode": "04",
-    "dateCreated": "2017-06-13 09:48:26",
-    "lastUpdated": "2017-06-13 09:48:26",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
     "isFeatured": false,
     "images": [
         {
@@ -126,188 +17,48 @@ module.exports = [
             "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/apparel-nav.svg",
             "imageType": "NAV_ICON"
         }
+    ],
+    children: [
+      {
+        "name": "Shorts",
+        "description": "Shorts",
+        "categoryCode": "05",
+        "isFeatured": false,
+        "images": [
+            {
+                "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/shorts-bg.jpg",
+                "imageType": "BACKGROUND"
+            },
+            {
+                "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/shorts-main.svg",
+                "imageType": "MAIN"
+            },
+            {
+                "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/shorts-nav.svg",
+                "imageType": "NAV_ICON"
+            }
+        ]
+      },
+      {
+        "name": "Shirt",
+        "description": "Shirt",
+        "categoryCode": "07",
+        "isFeatured": false,
+        "images": [
+            {
+                "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/shirt-bg.jpg",
+                "imageType": "BACKGROUND"
+            },
+            {
+                "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/shirt-main.svg",
+                "imageType": "MAIN"
+            },
+            {
+                "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/shirt-nav.svg",
+                "imageType": "NAV_ICON"
+            }
+        ]
+      }
     ]
-  },
-  {
-    "id": 6,
-    "name": "Novelty",
-    "description": "Novelty",
-    "categoryCode": "06",
-    "dateCreated": "2017-06-13 09:49:39",
-    "lastUpdated": "2017-06-13 09:49:39",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
-    "isFeatured": false
-  },
-  {
-    "id": 7,
-    "name": "Laundry and Home Care",
-    "description": "Laundry and Home Care",
-    "categoryCode": "07",
-    "dateCreated": "2017-06-13 09:52:49",
-    "lastUpdated": "2017-06-13 09:52:49",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
-    "isFeatured": false
-  },
-  {
-    "id": 8,
-    "name": "Health and Beauty",
-    "description": "Health and Beauty",
-    "categoryCode": "08",
-    "dateCreated": "2017-06-13 09:53:32",
-    "lastUpdated": "2017-06-13 09:53:32",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
-    "isFeatured": false
-  },
-  {
-    "id": 9,
-    "name": "Gadgets",
-    "description": "Gadgets",
-    "categoryCode": "09",
-    "dateCreated": "2017-06-13 10:06:54",
-    "lastUpdated": "2017-06-13 10:06:54",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
-    "isFeatured": false,
-    "images": [
-        {
-            "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/gadgets-bg.jpg",
-            "imageType": "BACKGROUND"
-        },
-        {
-            "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/gadgets-nav.svg",
-            "imageType": "NAV_ICON"
-        },
-        {
-            "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/gadgets-main.svg",
-            "imageType": "MAIN"
-        }
-    ]
-  },
-  {
-    "id": 10,
-    "name": "Watches",
-    "description": "Watches",
-    "categoryCode": "10",
-    "dateCreated": "2017-06-14 16:53:12",
-    "lastUpdated": "2017-06-14 16:53:12",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
-    "isFeatured": false
-  },
-  {
-    "id": 12,
-    "name": "Home & Living",
-    "description": "Home & Living",
-    "categoryCode": "12",
-    "dateCreated": "2017-06-14 16:59:40",
-    "lastUpdated": "2017-06-14 16:59:40",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
-    "isFeatured": false
-  },
-  {
-    "id": 13,
-    "name": "Fragrances",
-    "description": "Fragrances",
-    "categoryCode": "13",
-    "dateCreated": "2017-06-15 18:11:52",
-    "lastUpdated": "2017-06-15 18:11:52",
-    "createdBy": "711admin",
-    "updatedBy": "711admin",
-    "isFeatured": false,
-    "images": [
-        {
-            "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/fragrances-bg.jpg",
-            "imageType": "BACKGROUND"
-        },
-        {
-            "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/fragrances-nav.svg",
-            "imageType": "NAV_ICON"
-        },
-        {
-            "imageUrl": "https://cliqq.imgix.net/test/category/CategoriesAssets/fragrances-main.svg",
-            "imageType": "MAIN"
-        }
-    ]
-  },
-  {
-    "id": 14,
-    "name": "Oral Care",
-    "description": "Oral Care",
-    "categoryCode": "14",
-    "dateCreated": "2017-06-19 17:24:41",
-    "lastUpdated": "2017-06-19 17:24:41",
-    "createdBy": "ailene",
-    "updatedBy": "ailene",
-    "isFeatured": false
-  },
-  {
-    "id": 15,
-    "name": "Deodorants",
-    "description": "Deodorants",
-    "categoryCode": "15",
-    "dateCreated": "2017-06-19 17:32:07",
-    "lastUpdated": "2017-06-19 17:32:07",
-    "createdBy": "ailene",
-    "updatedBy": "ailene",
-    "isFeatured": false
-  },
-  {
-    "id": 16,
-    "name": "Hair Care",
-    "description": "Hair Care",
-    "categoryCode": "16",
-    "dateCreated": "2017-06-19 17:34:19",
-    "lastUpdated": "2017-06-19 17:34:19",
-    "createdBy": "ailene",
-    "updatedBy": "ailene",
-    "isFeatured": false
-  },
-  {
-    "id": 17,
-    "name": "Skin Care",
-    "description": "Skin Care",
-    "categoryCode": "17",
-    "dateCreated": "2017-06-19 17:34:30",
-    "lastUpdated": "2017-06-19 17:34:30",
-    "createdBy": "ailene",
-    "updatedBy": "ailene",
-    "isFeatured": false
-  },
-  {
-    "id": 18,
-    "name": "Food",
-    "description": "Food",
-    "categoryCode": "18",
-    "dateCreated": "2017-07-25 09:14:13",
-    "lastUpdated": "2017-07-25 09:14:13",
-    "createdBy": "ailene",
-    "updatedBy": "ailene",
-    "isFeatured": false
-  },
-  {
-    "id": 19,
-    "name": "Sports & Travel",
-    "description": "Sports & Travel",
-    "categoryCode": "19",
-    "dateCreated": "2017-08-03 10:16:19",
-    "lastUpdated": "2017-09-14 11:17:11",
-    "createdBy": "ailene",
-    "updatedBy": "711admin",
-    "isFeatured": false
-  },
-  {
-    "id": 21,
-    "name": "Beauty",
-    "description": "Beauty",
-    "categoryCode": "21",
-    "dateCreated": "2017-11-20 18:52:20",
-    "lastUpdated": "2017-11-20 18:52:20",
-    "createdBy": "admin",
-    "updatedBy": "admin",
-    "isFeatured": false
   }
-  ]
+]
